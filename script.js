ca7ba2f9b2f3c4b61dcd26938844950b409a8077
jaketux <169253660+jaketux@users.noreply.github.com>
2024-10-29 08:34:59 +1100

    Working on JS
    I have added partial functionality to the calc, it is storing numbers in arrays and then joining them together.

--- script.js
+++ script.js
@@ -1,8 +1,29 @@
+const numberButtons = document.querySelectorAll(".number-btn")
+const operatorButtons = document.querySelectorAll(".operator-btn")
+const calculatorScreenText = document.querySelector(".calculator-screen-text")
 
+numberButtons.forEach((numberButton)=>{numberButton.addEventListener("click",function(e){
+    enteringFirstNumber.push(this.innerText);
+console.log(enteringFirstNumber)})})
+
+operatorButtons.forEach((operatorButton)=>{operatorButton.addEventListener("click",function(e){
+    let numberStored=Number(enteringFirstNumber.join(""));
+    console.log(numberStored)
+    calculatorScreenText.textContent=numberStored;
+    enteringFirstNumber.length=0
+
+})})
+
+let numberStored=0
 let firstNumber = 0
 let secondNumber = 0
 let operator = 0
 let outcome = 0
+
+
+let enteringFirstNumber=[]
+
+let finalFirstNumber=Number(enteringFirstNumber.join(""))
 
 function operate(operator,firstNumber,secondNumber){
     if (operator=="+"){

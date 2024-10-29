11b723afcef1e4043687110ee9fb72daeb644d66
jaketux <169253660+jaketux@users.noreply.github.com>
2024-10-29 22:30:13 +1100

    Improved functionality
    Calculator is now working for single operations, I am able to complete calculations using number + operator + number equals.
    
    I now need to add the functionality to chain these functions so that the calculation completes by selecting another operator instead of having to hit equals.

--- script.js
+++ script.js
@@ -1,46 +1,113 @@
 const numberButtons = document.querySelectorAll(".number-btn")
-const operatorButtons = document.querySelectorAll(".operator-btn")
+const addButton = document.querySelector(".add-btn")
+const subtractButton = document.querySelector(".subtract-btn")
+const multiplyButton = document.querySelector(".multiply-btn")
+const divideButton = document.querySelector(".divide-btn")
+const equalsButton = document.querySelector(".equals-btn")
 const calculatorScreenText = document.querySelector(".calculator-screen-text")
+const clearButton = document.querySelector(".clear-btn")
 
-numberButtons.forEach((numberButton)=>{numberButton.addEventListener("click",function(e){
-    enteringFirstNumber.push(this.innerText);
-console.log(enteringFirstNumber)})})
-
-operatorButtons.forEach((operatorButton)=>{operatorButton.addEventListener("click",function(e){
-    let numberStored=Number(enteringFirstNumber.join(""));
-    console.log(numberStored)
-    calculatorScreenText.textContent=numberStored;
-    enteringFirstNumber.length=0
-
-})})
+let enteringFirstNumber=[]
 
 let numberStored=0
 let firstNumber = 0
 let secondNumber = 0
 let operator = 0
 let outcome = 0
-
-
-let enteringFirstNumber=[]
-
 let finalFirstNumber=Number(enteringFirstNumber.join(""))
 
-function operate(operator,firstNumber,secondNumber){
-    if (operator=="+"){
-        let totalSum = firstNumber+secondNumber;
-        return totalSum
+
+
+numberButtons.forEach((numberButton)=>{numberButton.addEventListener("click",function(e){
+    enteringFirstNumber.push(this.innerText);
+    calculatorScreenText.innerText += numberButton.innerText;
+    addButton.classList.remove("operatorinuse")
+    subtractButton.classList.remove("operatorinuse")
+    multiplyButton.classList.remove("operatorinuse")
+    divideButton.classList.remove("operatorinuse")
+
+console.log(enteringFirstNumber)})})
+
+addButton.addEventListener("click",function(e){
+    numberStored=Number(enteringFirstNumber.join(""));
+    operator="+"
+    calculatorScreenText.innerText=""
+    addButton.classList.add("operatorinuse")
+    enteringFirstNumber.length=0;
+    console.log(numberStored)
+})
+
+subtractButton.addEventListener("click",function(e){
+    numberStored=Number(enteringFirstNumber.join(""));
+    operator="-"
+    calculatorScreenText.innerText=""
+    subtractButton.classList.add("operatorinuse")
+    enteringFirstNumber.length=0
+    console.log(numberStored)    
+})
+multiplyButton.addEventListener("click",function(e){
+    numberStored=Number(enteringFirstNumber.join(""));
+    operator="*"
+    calculatorScreenText.innerText=""
+    multiplyButton.classList.add("operatorinuse")
+    enteringFirstNumber.length=0
+    console.log(numberStored)      
+})
+
+divideButton.addEventListener("click",function(e){
+    numberStored=Number(enteringFirstNumber.join(""));
+    operator="/"
+    calculatorScreenText.innerText=""
+    divideButton.classList.add("operatorinuse")
+    enteringFirstNumber.length=0
+    console.log(numberStored)      
+})
+
+clearButton.addEventListener("click",function(e){
+    numberStored=0;
+    operator=0
+    secondNumber=0
+    firstNumber=0
+    addButton.classList.remove("operatorinuse")
+    subtractButton.classList.remove("operatorinuse")
+    multiplyButton.classList.remove("operatorinuse")
+    divideButton.classList.remove("operatorinuse")
+    calculatorScreenText.innerText=""
+    enteringFirstNumber.length=0;
+})
+
+equalsButton.addEventListener("click",function(e){
+    function operate(operator,firstNumber,secondNumber){
+        if (operator=="+"){
+            let total = firstNumber+secondNumber;
+            return outcome=total
+        }
+        else if (operator=="-"){
+            let total = firstNumber-secondNumber;
+            return outcome=total
+        }
+        else if (operator=="/"){
+            if (secondNumber === 0) {
+                return outcome="ERROR";
+            }
+            let total = firstNumber/secondNumber;
+            return outcome=total
+        }
+        else if (operator=="*"){
+            let total = firstNumber*secondNumber;
+            return outcome=total;
+        }
     }
-    else if (operator=="-"){
-        let totalSubtract = firstNumber-secondNumber;
-        return totalSubtract
-    }
-    else if (operator=="/"){
-        let totalDivide = firstNumber/secondNumber;
-        return totalDivide
-    }
-    else if (operator=="*"){
-        let totalMultiply = firstNumber*secondNumber;
-        return totalMultiply;
-    }
-}
+
+    let firstNumber=numberStored
+    let secondNumber=Number(enteringFirstNumber.join(""))
+    operate(operator,firstNumber,secondNumber)
+    calculatorScreenText.innerText=outcome
+    console.log(outcome)
+})
+
+
+
+
+
 
